aea199d7b36d454d54627dfc1034c9f02569ebcb
Jessie Douglas <jessielou.douglas@gmail.com>
2019-06-16 12:25:48 -0400

    Fix seating arrangments to work with tables

--- handlers/all_seating.py
+++ handlers/all_seating.py
@@ -6,7 +6,6 @@ from google.appengine.ext import ndb
 from google.appengine.api import users
 from models import classroom
 from models import student
-from models import seating_arrangement
 from models import table
 from utils import rendering_util
 from utils import seating_util
@@ -16,13 +15,17 @@ env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 class AllSeatingHandler(webapp2.RequestHandler):
     UNOWNED_RESOURCE_ERROR = 'Sorry, the requested classroom does not belong to you.'
 
-    def _render(self, c=None, seating_arrangements=[], msg=None):
+    def _render(self, c=None, seating_arrangements=None,
+                arrangement_key_to_tables=None, msg=None):
         template = env.get_template('all_arrangements.html')
         vars_dict = {
             "msg": msg,
             "classroom": c,
             "classrooms": seating_util.get_classrooms(),
-            "seating_arrangements": seating_arrangements,
+            "seating_arrangements":
+                seating_arrangements if seating_arrangements else [],
+            "arrangement_key_to_tables":
+                arrangement_key_to_tables if arrangement_key_to_tables else {},
         }
         main_content = template.render(vars_dict)
         self.response.out.write(rendering_util.render_page(main_content))
@@ -39,12 +42,25 @@ class AllSeatingHandler(webapp2.RequestHandler):
             key = ndb.Key(urlsafe=urlsafe)
             c = key.get()
 
-        seating_arrangements = []
-
         if c and c.user_id != current_user.user_id():
             self._render(msg=UNOWNED_RESOURCE_ERROR)
 
-        if c:
-            seating_arrangements = seating_util.get_seating_arrangements(c)
+        seating_arrangements = (
+            seating_util.get_seating_arrangements(c) if c else [])
+        arrangement_key_to_tables = {
+            a.key:self._get_tables_and_students(a) \
+            for a in seating_arrangements
+        }
 
-        self._render(c=c, seating_arrangements=seating_arrangements)
+        self._render(c=c, seating_arrangements=seating_arrangements,
+                     arrangement_key_to_tables=arrangement_key_to_tables)
+
+    def _get_tables_and_students(self, arrangement):
+        '''Retrieves all tables and students for a given arrangement.'''
+        tables = ndb.get_multi(arrangement.tables)
+        all_students = ndb.get_multi([s for t in tables for s in t.students])
+        keys_to_students = {s.key:s for s in all_students}
+        return [[
+                keys_to_students.get(key) for key in t.students
+            ] for t in tables
+        ]
--- utils/seating_util.py
+++ utils/seating_util.py
@@ -3,6 +3,8 @@ from models import classroom
 from models import seating_arrangement
 
 def get_seating_arrangements(current_classroom):
+    '''Returns a list of seating arrangements for the given classroom ordered by
+    day.'''
     query = seating_arrangement.SeatingArrangement.query()
     filtered = query.filter(
             seating_arrangement.SeatingArrangement.classroom==current_classroom.key)
@@ -11,6 +13,7 @@ def get_seating_arrangements(current_classroom):
     return seating_arrangements
 
 def get_classrooms():
+    '''Returns a list of all classrooms for the current user.'''
     return classroom.Classroom.query().filter(
         classroom.Classroom.user_id==users.get_current_user().user_id()
     ).fetch()
